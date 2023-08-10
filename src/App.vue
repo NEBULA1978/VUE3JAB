1b6180c25df76685b1f7339370c1656fd7d6f42f
NEBULA1978 <rpascualey@gmail.com>
2023-08-10 12:26:44 +0200

    PRPS,EMIT e IYECCION para cambiar valores de texto con un boton

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Padre:</h1>
+    <HijoVue :nombre="nombre" :edad="edad" @modificar="cambiarValor" />
+    <HijoVue2 />
+  </div>
+</template>
+
+<script setup>
+import { provide, ref } from "vue";
+import HijoVue from "./components/Hijo.vue";
+import HijoVue2 from "./components/Hijo2.vue";
+
+const cambiarValor = (valor1, valor2) => {
+  nombre.value = valor1;
+  edad.value = valor2;
+}
+const nombre = ref('Jab');
+const edad = ref(18);
+const canal = ref("soyjab")
+provide('miCanal', canal)
+</script>
+
+<style scoped></style>
