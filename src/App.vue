aa0e4a54fc69cc47778d9f47735936b1225cf434
NEBULA1978 <rpascualey@gmail.com>
2023-08-10 18:19:54 +0200

    Creando botones de navegacion y responsive

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
-  <div>
-    <!-- <h1>Padre:</h1> -->
+  <div class="app-container">
     <router-view />
-
     <div class="buttons">
-      <router-link to="/nosotros">
+      <router-link to="/nosotros" class="nav-button">
         <button>Nosotros</button>
       </router-link>
-      <router-link to="/quienes-somos">
+      <router-link to="/quienes-somos" class="nav-button">
         <button>Quiénes Somos</button>
       </router-link>
     </div>
@@ -16,12 +14,39 @@
 
 <script setup>
 import { useRouter } from 'vue-router';
-
 const router = useRouter();
 </script>
 
 <style scoped>
+.app-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: #f0f0f0;
+}
+
 .buttons {
   margin-top: 20px;
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+}
+
+.nav-button {
+  text-decoration: none;
+  display: inline-block;
+  background-color: #007BFF;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.nav-button:hover {
+  background-color: #0056b3;
 }
 </style>
--- src/components/Nosotros.vue
+++ src/components/Nosotros.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
-        <h2>Nosotros</h2>
-        <!-- Contenido de la página Nosotros -->
+    <div class="page-container">
+        <h2>Quiénes Somos</h2>
+        <!-- Contenido de la página Quiénes Somos -->
 
-        <router-link to="/">
+        <router-link to="/" class="back-button">
             <button>Volver a la Página de Inicio</button>
         </router-link>
     </div>
@@ -12,4 +12,16 @@
 <script setup>
 </script>
 
-<style scoped></style>
+<style scoped>
+.page-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+}
+
+.back-button {
+    margin-top: 20px;
+}
+</style>
