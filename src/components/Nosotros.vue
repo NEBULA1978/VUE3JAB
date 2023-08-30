02a65aaeb9d99cd7fb354104f65167a8deee46d8
NEBULA1978 <rpascualey@gmail.com>
2023-08-30 19:50:26 +0200

    Cambiando contenido para boton Nossotros

--- src/components/Nosotros.vue
+++ src/components/Nosotros.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="page-container">
-        <h2>Quiénes Somos</h2>
+        <h2>Nosotros Somos</h2>
         <!-- Contenido de la página Quiénes Somos -->
 
         <router-link to="/" class="back-button">
