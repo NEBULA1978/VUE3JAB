44b75b692f8f90fd3b0266a2b43ced68d92ba3a5
NEBULA1978 <rpascualey@gmail.com>
2023-08-30 21:30:41 +0200

    Añadiendo en QuienesSomos.vue con v-on creando evento al pulsar muestra por consola Hola y el nombre Jab añado comentarios forma abreciada v-bind y v-on añado otra boton con otra pagina añado contador

--- src/components/Contacto.vue
+++ src/components/Contacto.vue
@@ -6,10 +6,44 @@
         <router-link to="/">
             <button>Volver a la Página de Inicio</button>
         </router-link>
+        <div class="counter-container">
+            <button @click="decrement">Decrementar</button>
+            <span>{{ contador }}</span>
+            <button @click="increment">Incrementar</button>
+        </div>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const contador = ref(0);
+
+const increment = () => {
+    contador.value++;
+};
+
+const decrement = () => {
+    contador.value--;
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.counter-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+}
+
+button {
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+}
+
+span {
+    margin: 0 20px;
+    font-size: 24px;
+}
+</style>
