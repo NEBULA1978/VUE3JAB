6e960084824a22fabf627efa203fac73de74c5bb
NEBULA1978 <rpascualey@gmail.com>
2023-08-30 20:06:13 +0200

    Añado componente padre y dos hijos y modifico App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,30 @@
         <button>Quiénes Somos</button>
       </router-link>
     </div>
+    <!-- Agrega los componentes aquí -->
+    <div>
+      <h1>Padre:</h1>
+      <HijoVue :nombre="nombre" :edad="edad" @modificar="cambiarValor" />
+      <HijoVue2 />
+    </div>
   </div>
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router';
+import { provide, ref } from "vue";
+import HijoVue from "./components/Hijo.vue";
+import HijoVue2 from "./components/Hijo2.vue";
+
+const cambiarValor = (valor1, valor2) => {
+  nombre.value = valor1;
+  edad.value = valor2;
+}
+const nombre = ref('Jab');
+const edad = ref(18);
+const canal = ref("soyjab")
+provide('miCanal', canal)
+
 const router = useRouter();
 </script>
 
