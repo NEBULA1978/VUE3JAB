885b979e92f9db3ef1a53b5f8e761237ce774c9b
NEBULA1978 <rpascualey@gmail.com>
2023-08-30 20:45:06 +0200

    Añadiendo en QuienesSomos.vue con v-bind:class creo variable en script para cambiar color del texto h2 de forma dinamica

--- src/components/Nosotros.vue
+++ src/components/Nosotros.vue
@@ -8,21 +8,35 @@
         </router-link>
     </div>
     <!-- No podemos poner dentro de <h2 class="" id="" style="" -->
-        <!-- Utilizamos las directivas -->
-    <h2 v-bind:style="miColor">
-        Hola Mundo VUE3 con {{ miNombre }} y tengo {{ miEdad+3 }}
-    </h2>
-
-</template>
+        <!-- Utilizamos las directivas por estilo-->
+        <h2 v-bind:style="miColor">
+            Hola Mundo VUE3 con {{ miNombre }} y tengo {{ miEdad+3 }}
+        </h2>
+        
+        
+        <!-- Utilizamos las directivas por clase-->
+        <h3 v-bind:class="selector">
+            Hola Mundo VUE3 con {{ miNombre }} y tengo {{ miEdad+3 }}
+        </h3>
+        
+    </template>
 
 <script setup>
 const miNombre="Jab"
 const miEdad=18
+
+// <!-- Utilizamos las directivas por style-->
 const miColor="color:green;font-size:3em;"
+
+// <!-- Utilizamos las directivas por clase-->
+const selector="azul"
 </script>
 
 <style scoped>
 /* h2{
   color:red;
 } */
+.azul{
+    color:blue;
+}
 </style>
