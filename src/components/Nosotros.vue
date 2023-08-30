0ff44d0466232ba451af475ce45871d0611be529
NEBULA1978 <rpascualey@gmail.com>
2023-08-30 20:57:38 +0200

    Añadiendo en QuienesSomos.vue con v-on creando evento al pulsar muestra por consola Hola y el nombre Jab

--- src/components/Nosotros.vue
+++ src/components/Nosotros.vue
@@ -10,10 +10,10 @@
     <!-- No podemos poner dentro de <h2 class="" id="" style="" -->
         <!-- Utilizamos las directivas por estilo-->
         <h2 v-bind:style="miColor">
-            Hola Mundo VUE3 con {{ miNombre }} y tengo {{ miEdad+3 }}
+            Hola Mundo VUE3 con {{ nombre }} y tengo {{ miEdad+3 }}
         </h2>
         
-        
+        <button v-on:click="saludar(miNombre)">Aceptar</button>
         <!-- Utilizamos las directivas por clase-->
         <h3 v-bind:class="selector">
             Hola Mundo VUE3 con {{ miNombre }} y tengo {{ miEdad+3 }}
@@ -30,6 +30,12 @@ const miColor="color:green;font-size:3em;"
 
 // <!-- Utilizamos las directivas por clase-->
 const selector="azul"
+
+// Evento boton
+const saludar=(nombre)=>{
+    console.log("Hola")
+    console.log(`Hola ${nombre}`)
+}
 </script>
 
 <style scoped>
